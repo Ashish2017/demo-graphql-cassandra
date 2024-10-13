4c48e082c9604a46155fa682a81cddeeabddfcc3
Ashish Senapati <ashishsenapati20@gmail.com>
2024-10-13 19:39:42 +0530

    first commit

---
+++ src/main/java/com/senappa/demo_graphql_cassandra/DemoGraphqlCassandraApplication.java
@@ -0,0 +1,13 @@
+package com.senappa.demo_graphql_cassandra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGraphqlCassandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGraphqlCassandraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senappa/demo_graphql_cassandra/DemoGraphqlCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senappa.demo_graphql_cassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGraphqlCassandraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
